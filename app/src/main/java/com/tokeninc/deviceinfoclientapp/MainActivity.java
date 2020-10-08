6ab2349c6a063e6104e6b2ac31732ce53fdcfccc
devops21 <devops.smtp.21@gmail.com>
2020-10-08 10:29:02 +0300

    deviceinfoclientapp project initial commit

---
+++ app/src/main/java/com/tokeninc/deviceinfoclientapp/MainActivity.java
@@ -0,0 +1,127 @@
+package com.tokeninc.deviceinfoclientapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.tokeninc.deviceinfo.DeviceInfo;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView fiscalIdText, modemVersionText, lynxVersionText, uptVersionText, imeiNumberText, imsiNumberText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lynxVersionText = findViewById(R.id.lynxVersion);
+        uptVersionText = findViewById(R.id.uptVersion);
+        fiscalIdText = findViewById(R.id.fiscalID);
+        modemVersionText = findViewById(R.id.modemVersion);
+        imeiNumberText = findViewById(R.id.imeiNumber);
+        imsiNumberText = findViewById(R.id.imsiNumber);
+
+        DeviceInfo deviceInfo = new DeviceInfo(this);
+
+        // get FiscalId
+        deviceInfo.getFiscalId(new DeviceInfo.DeviceInfoResponseHandler() {
+            @Override
+            public void onSuccess(String result) {
+                fiscalIdText.setText(result);
+            }
+
+            @Override
+            public void onFail(String errMessage) {
+                fiscalIdText.setText(errMessage);
+            }
+        });
+
+        // get IMEI number
+        deviceInfo.getImeiNumber(new DeviceInfo.DeviceInfoResponseHandler() {
+            @Override
+            public void onSuccess(String result) {
+                imeiNumberText.setText(result);
+            }
+
+            @Override
+            public void onFail(String errMessage) {
+                imeiNumberText.setText(errMessage);
+            }
+        });
+
+        // get IMSI number
+        deviceInfo.getImsiNumber(new DeviceInfo.DeviceInfoResponseHandler() {
+            @Override
+            public void onSuccess(String result) {
+                imsiNumberText.setText(result);
+            }
+
+            @Override
+            public void onFail(String errMessage) {
+                imsiNumberText.setText(errMessage);
+            }
+        });
+
+        // get Modem version
+        deviceInfo.getModemVersion(new DeviceInfo.DeviceInfoResponseHandler() {
+            @Override
+            public void onSuccess(String result) {
+                modemVersionText.setText(result);
+            }
+
+            @Override
+            public void onFail(String errMessage) {
+                modemVersionText.setText(errMessage);
+            }
+        });
+
+        // get Lynx version
+        deviceInfo.getLynxVersion(new DeviceInfo.DeviceInfoResponseHandler() {
+            @Override
+            public void onSuccess(String result) {
+                lynxVersionText.setText(result);
+            }
+
+            @Override
+            public void onFail(String errMessage) {
+                lynxVersionText.setText(errMessage);
+            }
+        });
+
+        // get Upt version
+        deviceInfo.getUptVersion(new DeviceInfo.DeviceInfoResponseHandler() {
+            @Override
+            public void onSuccess(String result) {
+                uptVersionText.setText(result);
+            }
+
+            @Override
+            public void onFail(String errMessage) {
+                uptVersionText.setText(errMessage);
+            }
+        });
+
+
+        // to get multiple fields in a single callback, use getFields method
+        deviceInfo.getFields(
+
+                fields -> {
+                    if (fields == null) return;
+                    // fields is the string array that contains info in the requested order
+                    Log.i("Example 2", "Fiscal ID:   " + fields[0]);
+                    Log.i("Example 2", "IMEI Number: " + fields[1]);
+                    Log.i("Example 2", "IMSI Number: " + fields[2]);
+                },
+
+                // write requested fields
+                DeviceInfo.Field.FISCAL_ID, DeviceInfo.Field.IMEI_NUMBER, DeviceInfo.Field.IMSI_NUMBER
+        );
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,63 @@
+## Introduction
+
+This is an example application to demonstrate the usage of DeviceInfo Service. 
+
+## Usage
+
+#### Include  the library in your project
+
+ 1) Copy DeviceInfo.aar from /app/libs folder into your project  
+ 2) Make sure you include aar lib. In this example project, it is done in /app/.build.gradle file like this:
+
+```groovy
+dependencies {
+    implementation fileTree(dir: "libs", include: ["*.jar", "*.aar"])
+    ...
+}
+```
+<br/>
+#### Reading Device info
+
+Create an instance of DeviceInfo class. It is recommended to reuse this instance when possible instead of creating a new one each time.
+
+```java
+DeviceInfo deviceInfo = new DeviceInfo(this);
+```
+
+<br/>
+In order to get a particular field, call related function on deviceInfo object.
+
+```java
+
+deviceInfo.getImeiNumber(new DeviceInfo.DeviceInfoResponseHandler() {
+    @Override
+    public void onSuccess(String result) {
+        imeiNumberText.setText(result);
+    }
+
+    @Override
+    public void onFail(String errMessage) {
+        imeiNumberText.setText(errMessage);
+    }
+});
+```
+
+<br/>
+To get multiple fields with a single call, use getFields method.
+
+```java
+
+deviceInfo.getFields(
+
+        fields -> {
+            if (fields == null) return;
+            // fields is the string array that contains info in the requested order
+            Log.i("Example 2", "Fiscal ID:   " + fields[0]);
+            Log.i("Example 2", "IMEI Number: " + fields[1]);
+            Log.i("Example 2", "IMSI Number: " + fields[2]);
+        },
+
+        // write requested fields
+        DeviceInfo.Field.FISCAL_ID, DeviceInfo.Field.IMEI_NUMBER, DeviceInfo.Field.IMSI_NUMBER
+);
+```
